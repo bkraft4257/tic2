76f25a1c492a54e76360b6da8366d1c144aa93d9
bkraft <bob.kraft@infiniteleap.net>
2018-01-19 12:18:57 -0500

    Updated HFPEF to decouple aliases.sh and environment.sh

---
+++ studies/hfpef/environment.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Environment variables
+
+export HFPEF_PATH=/gandg/hfpef/
+export HFPEF_BIDS_PATH=${HFPEF_PATH}/bids
+export HFPEF_IMAGE_ANALYSIS_PATH=${HFPEF_PATH}/image_analysis
+export HFPEF_IMAGE_PROCESSING_PATH=${HFPEF_PATH}/image_processing
+export HFPEF_SCRIPTS_PATH=${HFPEF_PATH}/scripts
+export HFPEF_MRIQC_PATH=${HFPEF_PATH}/mriqc
+
+export HFPEF_FMRIPREP_PATH=${HFPEF_IMAGE_PROCESSING_PATH}/fmriprep
+export HFPEF_NETPREP_PATH=${HFPEF_IMAGE_PROCESSING_PATH}/netprep
+
+export HFPEF_BIDS_CONFIG_FILE=${HFPEF_SCRIPTS_PATH}/hfpef_bids.cfg
--- studies/hfpef/hfpef_init.sh
+++ studies/hfpef/hfpef_init.sh
@@ -4,7 +4,7 @@
 # HFPEF Study initialization ====================================================
 
 export HFPEF_PATH=/gandg/hfpef
-source ${TIC_PATH}/studies/hfpef/init.sh
 
-
+source ${TIC_PATH}/studies/hfpef/aliases.sh
+source ${TIC_PATH}/studies/hfpef/environment.sh
 
