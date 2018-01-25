b1dde7357eeb26efada1c4a35b0ceab6275d192f
bkraft <bob.kraft@infiniteleap.net>
2018-01-25 17:30:20 -0500

    Updated act.sh - still having trouble with redirect. Created mriqc

--- bin/mriqc.sh
+++ bin/mriqc.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
-ACTIVE_ACT_OUTPUT_PATH=$ACTIVE_IMAGE_PROCESSING_PATH/act
-
-echo
-echo 'active study = ' $ACTIVE_STUDY
-echo 'bids path    = ' $ACTIVE_BIDS_PATH
-echo 'log path     = ' $ACTIVE_IMAGE_PROCESSING_LOG_PATH
-echo 'output path  = ' $ACTIVE_ACT_OUTPUT_PATH
-echo
+ACTIVE_APP_OUTPUT_PATH=$ACTIVE_MRIQC_PATH
+APP_SINGULARITY_IMAGE=$MRIQC_SINGULARITY_IMAGE
 
 # Convert to lower case
+app=mriqc
 study_prefix=$(echo "${ACTIVE_STUDY,,}")
 
 # create the output and work directories parallel to BIDS hierarchy, not inside it
 
 datetime_stamp=`date '+d%Y%m%d_%H:%M:%S'`
-log_file=${ACTIVE_IMAGE_PROCESSING_LOG_PATH}/${study_prefix}_act_${datetime_stamp}.log
+log_file=${ACTIVE_IMAGE_PROCESSING_LOG_PATH}/${study_prefix}_${app}_${datetime_stamp}.log
 
 echo
-echo $log_file
+echo 'active study           = ' $ACTIVE_STUDY
+echo 'bids path              = ' $ACTIVE_BIDS_PATH
+echo 'log path               = ' $ACTIVE_IMAGE_PROCESSING_LOG_PATH
+echo 'output path            = ' $ACTIVE_APP_OUTPUT_PATH
+echo 'app singularity image  = ' $APP_SINGULARITY_IMAGE
+echo 'log file               = ' $log_file
 echo
 
 # NOTE: any -B mount points must exist in the container
 #       run "sudo singularity shell -s xx.img"  and create the mount points
-
-ANTS_CORTICAL_THICKNESS_SINGULARITY_IMAGE='/cenc/software/bids_apps/antsCorticalThickness/bids_antscorticalthickness-2017-10-14-95aa110c26f8.img'
-
-echo 'singularity command   = ' $SINGULARITY_COMMAND
-echo 'act singularity image = ' $ANTS_CORTICAL_THICKNESS_SINGULARITY_IMAGE
-
 
 # https://askubuntu.com/questions/625224/how-to-redirect-stderr-to-a-file
 # Redirect stdout to one file and stderr to another file:
@@ -43,16 +37,16 @@ echo 'act singularity image = ' $ANTS_CORTICAL_THICKNESS_SINGULARITY_IMAGE
 
 # run it in the background so that it continues if user logs out
 act_full_command=$SINGULARITY_COMMAND \
-                 $ANTS_CORTICAL_THICKNESS_SINGULARITY_IMAGE \
+                 $APP_SINGULARITY_IMAGE \
                  $ACTIVE_BIDS_PATH \
-                 $ACTIVE_ACT_OUTPUT_PATH \
+                 $ACTIVE_APP_OUTPUT_PATH \
                  participant ${@} &> $log_file
 
 
-nohup time /usr/local/bin/singularity run -w -B /cenc -B /gandg -B /bkraft1 \
-$ANTS_CORTICAL_THICKNESS_SINGULARITY_IMAGE \
+/usr/local/bin/singularity run -w -B /cenc -B /gandg -B /bkraft1 \
+                 $APP_SINGULARITY_IMAGE \
                  $ACTIVE_BIDS_PATH \
-                 $ACTIVE_ACT_OUTPUT_PATH \
+                 $ACTIVE_APP_OUTPUT_PATH \
                  participant ${@} > $log_file 2>&1 &
 
 
