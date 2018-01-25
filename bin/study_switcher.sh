912fa91eb688cc8021468d2635da4bccdd0f6ec5
bkraft <bob.kraft@infiniteleap.net>
2018-01-25 14:11:09 -0500

    Added bash script to call study_switcher.

---
+++ bin/study_switcher.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+tic_active_study_bash_script=tic_study_switcher.txt
+
+echo "Previous active study : " $ACTIVE_STUDY
+
+study_switcher.py $1
+
+source $tic_active_study_bash_script
+rm $tic_active_study_bash_script
+
+echo "Current active study :" $ACTIVE_STUDY
+
