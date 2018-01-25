570be96df3bb2a8d3ef24d4633b1fa258a0524b9
bkraft <bob.kraft@infiniteleap.net>
2018-01-25 14:13:56 -0500

    Debugging on aging1a.

--- bin/study_switcher.sh
+++ bin/study_switcher.sh
@@ -7,7 +7,8 @@ echo "Previous active study : " $ACTIVE_STUDY
 study_switcher.py $@
 
 source $tic_active_study_bash_script
-rm $tic_active_study_bash_script
+# rm $tic_active_study_bash_script
 
 echo "Current active study :" $ACTIVE_STUDY
 
+term
