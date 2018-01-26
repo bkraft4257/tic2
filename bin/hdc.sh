50bcdf80952b3ef3a781a1dab69ef10b6b3692ba
bkraft <bob.kraft@infiniteleap.net>
2018-01-26 09:26:33 -0500

    Added code and environment variables to use the hdc.sh script across studies. Debugging on aging1a

--- bin/hdc.sh
+++ bin/hdc.sh
@@ -8,10 +8,11 @@ echo 'active study           = ' $ACTIVE_STUDY
 echo 'bids path              = ' $ACTIVE_BIDS_PATH
 echo
 
-hdc_command='$HDC_COMMAND -c dcm2niix -b --minmeta -f $ACTIVE_HEUDICONV_PROTOCOL -o $ACTIVE_BIDS_PATH -d "{subject}/2*/*/*.DCM"'
-
-echo $hdc_command
-
-$hdc_command
+/cenc/software/heudiconv/python/heudiconv/bin/heudiconv \
+          -c dcm2niix \
+          -b --minmeta \
+          -f $ACTIVE_HEUDICONV_PROTOCOL \
+          -o $ACTIVE_BIDS_PATH \
+          -d "{subject}/2*/*/*.DCM"
 
 
