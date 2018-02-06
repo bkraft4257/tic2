4ae453822193d8d8084be34a8ccab08eada9fdf9
bkraft <bob.kraft@infiniteleap.net>
2018-02-06 15:26:40 -0500

    Fixed bug.  environemnt.sh must be called before aliases.sh

--- studies/synergy/synergy_init.sh
+++ studies/synergy/synergy_init.sh
@@ -5,6 +5,6 @@
 
 export SYNERGY_PATH=/gandg/synergy
 
-source ${TIC_PATH}/studies/synergy/aliases.sh
 source ${TIC_PATH}/studies/synergy/environment.sh
-
+source ${TIC_PATH}/studies/synergy/aliases.sh
+˚
