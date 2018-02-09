f8fa85f028cf747efc364c96682a9134412e5600
bkraft <bob.kraft@infiniteleap.net>
2018-02-09 11:56:12 -0500

    fmriprep is not running. Debugging now.  Reverting back to fmriprep.sh from master.
    Throwing error
    
    ERROR  : No valid /bin/sh in container
    ABORT  : Retval = 255
    0.01user 0.00system 0:00.03elapsed 48%CPU (0avgtext+0avgdata 1484maxresident)k
    0inputs+8outputs (0major+1081minor)pagefaults 0swaps

--- bin/fmriprep.sh
+++ bin/fmriprep.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-BIDS_APP='fmriprep'
 
-ACTIVE_APP_OUTPUT_PATH=$ACTIVE_FMRIPREP_PATH
-ACTIVE_APP_SINGULARITY_IMAGE=$FMRIPREP_SINGULARITY_IMAGE
-ACTIVE_APP_WORKING_PATH=$ACTIVE_FMRIPREP_PATH/_working
+ACTIVE_APP_OUTPUT_PATH=$ACTIVE_MRIQC_PATH
+APP_SINGULARITY_IMAGE=$FMRIPREP_SINGULARITY_IMAGE
+BIDS_APP=fmriprep
 
 # Convert to lower case
 study_prefix=$(echo "${ACTIVE_STUDY,,}")
@@ -20,11 +19,9 @@ echo 'bids app               = ' $BIDS_APP
 echo 'bids path              = ' $ACTIVE_BIDS_PATH
 echo 'log path               = ' $ACTIVE_IMAGE_PROCESSING_LOG_PATH
 echo 'output path            = ' $ACTIVE_APP_OUTPUT_PATH
-echo 'working path           = ' $ACTIVE_APP_WORKING_PATH
-echo 'app singularity image  = ' $ACTIVE_APP_SINGULARITY_IMAGE
+echo 'app singularity image  = ' $APP_SINGULARITY_IMAGE
 echo 'log file               = ' $log_file
 echo
-
 
 # NOTE: any -B mount points must exist in the container
 #       run "sudo singularity shell -s xx.img"  and create the mount points
@@ -40,8 +37,17 @@ echo
 # command &> out
 
 
+# run it in the background so that it continues if user logs out
+full_command=$SINGULARITY_COMMAND \
+                 $APP_SINGULARITY_IMAGE \
+                 $ACTIVE_BIDS_PATH \
+                 $ACTIVE_APP_OUTPUT_PATH \
+                 participant ${@} &> $log_file
+
+
 nohup time /usr/local/bin/singularity run -w -B /cenc -B /gandg -B /bkraft1 \
                  $APP_SINGULARITY_IMAGE \
                  $ACTIVE_BIDS_PATH \
                  $ACTIVE_APP_OUTPUT_PATH \
                  participant ${@} > $log_file 2>&1 &
+
