1cd88f9caa5825114edf262631465dc006a97099
bkraft <bob.kraft@infiniteleap.net>
2018-02-21 13:32:42 -0500

    Fixed bug. Missing output directory in act.sh

--- bin/act.sh
+++ bin/act.sh
@@ -19,20 +19,6 @@ log_file=${ACTIVE_IMAGE_PROCESSING_LOG_PATH}/${study_prefix}_${BIDS_APP}_${datet
 
 source ${TIC_PATH}/studies/active/scripts/bids_app_status.sh
 
-# echo ' ' | tee log_file
-# echo 'datetime.now()         = ' $(date) | tee log_file
-# echo 'active study           = ' $ACTIVE_STUDY | tee log_file
-# echo 'bids app               = ' $BIDS_APP | tee log_file
-# echo 'bids path              = ' $ACTIVE_BIDS_PATH | tee log_file
-# echo 'log path               = ' $ACTIVE_IMAGE_PROCESSING_LOG_PATH | tee log_file
-# echo 'output path            = ' $ACTIVE_APP_OUTPUT_PATH | tee log_file
-# echo 'working path           = ' $ACTIVE_APP_WORKING_PATH | tee log_file
-# echo 'SUBJECTS_DIR           = ' $SUBJECTS_DIR | tee log_file
-# echo 'singularity command    = ' $SINGULARITY_COMMAND
-# echo 'app singularity image  = ' $APP_SINGULARITY_IMAGE | tee log_file
-# echo 'log file               = ' $log_file | tee log_file
-# echo ' ' | tee log_file
-
 
 # NOTE: any -B mount points must exist in the container
 #       run "sudo singularity shell -s xx.img"  and create the mount points
@@ -49,18 +35,23 @@ source ${TIC_PATH}/studies/active/scripts/bids_app_status.sh
 
 
 # run it in the background so that it continues if user logs out
-act_full_command=$SINGULARITY_COMMAND \
-                 $ANTS_CORTICAL_THICKNESS_SINGULARITY_IMAGE \
+cmd="act_full_command=$SINGULARITY_COMMAND \
+     $ANTS_CORTICAL_THICKNESS_SINGULARITY_IMAGE \
                  $ACTIVE_BIDS_PATH \
                  $ACTIVE_ACT_OUTPUT_PATH \
-                 participant ${@} &> $log_file
+                 participant ${@}"
 
+echo
+echo $cmd > $log_file
+echo
 
-nohup time /usr/local/bin/singularity run -w -B /cenc -B /gandg -B /bkraft1 \
-$ANTS_CORTICAL_THICKNESS_SINGULARITY_IMAGE \
-                 $ACTIVE_BIDS_PATH \
-                 $ACTIVE_ACT_OUTPUT_PATH \
-                 participant ${@} > $log_file 2>&1 &
+#nohup time /usr/local/bin/singularity run -w -B /cenc -B /gandg -B /bkraft1 \
+
+nohup time $SINGULARITY_COMMAND \
+           $APP_SINGULARITY_IMAGE \
+           $ACTIVE_BIDS_PATH \
+           $ACTIVE_APP_OUTPUT_PATH \
+           participant ${@} > $log_file 2>&1 &
 
 
 
