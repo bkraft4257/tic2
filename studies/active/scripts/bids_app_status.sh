412725b7ca4ce5ec198a358a32ba7f96f42302f4
bkraft <bob.kraft@infiniteleap.net>
2018-02-21 13:25:44 -0500

    Fixed writing to log_file

--- studies/active/scripts/bids_app_status.sh
+++ studies/active/scripts/bids_app_status.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-echo ' ' | tee log_file
-echo 'datetime.now()         = ' $(date) | tee log_file
-echo 'active study           = ' $ACTIVE_STUDY | tee log_file
-echo 'bids app               = ' $BIDS_APP | tee log_file
-echo 'bids path              = ' $ACTIVE_BIDS_PATH | tee log_file
-echo 'log path               = ' $ACTIVE_IMAGE_PROCESSING_LOG_PATH | tee log_file
-echo 'output path            = ' $ACTIVE_APP_OUTPUT_PATH | tee log_file
-echo 'working path           = ' $ACTIVE_APP_WORKING_PATH | tee log_file
-echo 'SUBJECTS_DIR           = ' $SUBJECTS_DIR | tee log_file
+echo ' ' | tee $log_file
+echo 'datetime.now()         = ' $(date) | tee $log_file
+echo 'active study           = ' $ACTIVE_STUDY | tee $log_file
+echo 'bids app               = ' $BIDS_APP | tee $log_file
+echo 'bids path              = ' $ACTIVE_BIDS_PATH | tee $log_file
+echo 'log path               = ' $ACTIVE_IMAGE_PROCESSING_LOG_PATH | tee $log_file
+echo 'output path            = ' $ACTIVE_APP_OUTPUT_PATH | tee $log_file
+echo 'working path           = ' $ACTIVE_APP_WORKING_PATH | tee $log_file
+echo 'SUBJECTS_DIR           = ' $SUBJECTS_DIR | tee $log_file
 echo 'singularity command    = ' $SINGULARITY_COMMAND
-echo 'app singularity image  = ' $APP_SINGULARITY_IMAGE | tee log_file
-echo 'log file               = ' $log_file | tee log_file
-echo ' ' | tee log_file
+echo 'app singularity image  = ' $APP_SINGULARITY_IMAGE | tee $log_file
+echo 'log file               = ' $$log_file | tee $log_file
+echo ' ' | tee $log_file
