003c72b2e6fe6284e39cc818b38b7efd42a818cb
bkraft <bob.kraft@infiniteleap.net>
2018-02-22 08:52:03 -0500

    Fixed bug. tee was overwriting log file not appending.

--- bin/fmriprep.sh
+++ bin/fmriprep.sh
@@ -17,21 +17,7 @@ parameters=${@}
 datetime_stamp=`date '+d%Y%m%d_%H:%M:%S'`
 log_file=${ACTIVE_IMAGE_PROCESSING_LOG_PATH}/${study_prefix}_${BIDS_APP}_${datetime_stamp}.log
 
-       $TIC_PATH/studies/active/scripts/bids_app_status.sh
 source $TIC_PATH/studies/active/scripts/bids_app_status.sh
-
-echo ' ' | tee $log_file
-echo 'datetime.now()         = ' $(date) | tee $log_file
-echo 'active study           = ' $ACTIVE_STUDY | tee $log_file
-echo 'bids app               = ' $BIDS_APP | tee $log_file
-echo 'bids path              = ' $ACTIVE_BIDS_PATH | tee $log_file
-echo 'log path               = ' $ACTIVE_IMAGE_PROCESSING_LOG_PATH | tee $log_file
-echo 'output path            = ' $ACTIVE_APP_OUTPUT_PATH | tee $log_file
-echo 'working path           = ' $ACTIVE_APP_WORKING_PATH | tee $log_file
-echo 'SUBJECTS_DIR           = ' $SUBJECTS_DIR | tee $log_file
-echo 'app singularity image  = ' $APP_SINGULARITY_IMAGE | tee $log_file
-echo 'log file               = ' $log_file | tee $log_file
-echo ' ' | tee $log_file
 
 # NOTE: any -B mount points must exist in the container
 #       run "sudo singularity shell -s xx.img"  and create the mount points
