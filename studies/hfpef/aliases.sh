720b95a67cc803ec5dc13c36ac2fd143fa965098
bkraft <bob.kraft@infiniteleap.net>
2018-03-08 14:20:28 -0500

    Simplified the hfpef aliases.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- studies/hfpef/aliases.sh
+++ studies/hfpef/aliases.sh
@@ -4,35 +4,21 @@
 alias rehfpef='source $HFPEF_SCRIPTS_PATH/aliases.sh'
 alias hfpef_help='firefox https://github.com/theimagingcollective/nipype_workflows/wiki/HFPEF-Image-Processing &'
 
-alias hfpef_bids='bids-validator $HFPEF_BIDS_PATH -c $HFPEF_BIDS_CONFIG_FILE'
-alias hfpef_bids_validitor='bids-validator $HFPEF_BIDS_PATH -c $HFPEF_BIDS_CONFIG_FILE'
-alias hfpef_bv='bids-validator $HFPEF_BIDS_PATH -c $HFPEF_BIDS_CONFIG_FILE'
-
-alias hfpef_hdc='/cenc/software/heudiconv/python/heudiconv/bin/heudiconv -c dcm2niix -b --minmeta -f /gandg/hfpef/scripts/hfpef_protocol_v2.py -o /gandg/hfpef/bids/ -d "{subject}/2*/*/*.DCM"'
-
-alias hfpef_display_bids='/gandg/hfpef/scripts/hfpef_display_bids.sh'
-alias hfpef_bids_display='/gandg/hfpef/scripts/hfpef_display_bids.sh'
-
-alias hfpef_clean_bids='/gandg/hfpef/scripts/hfpef_bids_clean.sh'
-alias hfpef_bids_clean='/gandg/hfpef/scripts/hfpef_bids_clean.sh'
+alias hfpef_clean_bids='$HFPEF_SCRIPTS_PATH/hfpef_bids_clean.sh'
+alias hfpef_bids_clean='$HFPEF_SCRIPTS_PATH/hfpef_bids_clean.sh'
 
 # The JSON file written after the upgrade is different than before the upgrade.
 # AcquisitionMatrixPE: 64 is no longer in the file and has been replaced with
 # AcquisitionNumber: 1.  I then modified the hfpef_clean_bids.sh file to accommodate this change. It is a quick and ugly
 # hack.
 
-alias hfpef_clean_bids_2b='/gandg/hfpef/scripts/hfpef_bids_clean_2b.sh'
-alias hfpef_bids_clean_2b='/gandg/hfpef/scripts/hfpef_bids_clean_2b.sh'
+alias hfpef_clean_bids_2b='$HFPEF_SCRIPTS_PATH/hfpef_bids_clean_2b.sh'
+alias hfpef_bids_clean_2b='$HFPEF_SCRIPTS_PATH/hfpef_bids_clean_2b.sh'
 
-alias hfpef_fmriprep='/gandg/hfpef/scripts/hfpef_fmriprep.sh'
-alias hfpef_mriqc='/gandg/hfpef/scripts/hfpef_mriqc.sh'
+alias hfpef_gi_netprep_epi='$HFPEF_SCRIPTS_PATH/hfpef_gi_netprep_epi.sh'
+alias hfpef_gi_netprep_mbepi='$HFPEF_SCRIPTS_PATH/hfpef_gi_netprep_mbepi.sh'
 
-alias hfpef_mriqc_group='mriqc  /gandg/hfpef/bids /gandg/hfpef/mriqc group'
-
-alias hfpef_gi_netprep_epi='/gandg/hfpef/scripts/hfpef_gi_netprep_epi.sh'
-alias hfpef_gi_netprep_mbepi='/gandg/hfpef/scripts/hfpef_gi_netprep_mbepi.sh'
-
-alias hfpef_gi_netprep='/gandg/hfpef/scripts/hfpef_gi_netprep.sh'
+alias hfpef_gi_netprep='$HFPEF_SCRIPTS_PATH/hfpef_gi_netprep.sh'
 alias hfpef_netprep='/gandg/tic/nipype_workflows/netprep.py'
 
 alias cdh='cd $HFPEF_PATH; lsreport_function'
