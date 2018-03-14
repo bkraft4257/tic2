8a0c61d75ca652edbd1028a3656c391ea3bc313f
bkraft <bob.kraft@infiniteleap.net>
2018-03-14 12:00:21 -0400

    Updated bids-validator and alias bv to be more expressive.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- bin/tic_bids_validator.sh
+++ bin/tic_bids_validator.sh
@@ -6,10 +6,14 @@ echo "==============="
 cat $ACTIVE_BIDS_PATH/.bidsignore;
 
 
+echo
+echo
 echo "cat .bids.cfg"
 echo "==============="
 cat $ACTIVE_BIDS_PATH/.bids.cfg;
 
+echo
+echo
 echo
 
 bids-validator -c $ACTIVE_BIDS_PATH/.bids.cfg $ACTIVE_BIDS_PATH ${@}
