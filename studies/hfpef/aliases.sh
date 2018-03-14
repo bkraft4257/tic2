f4f3af14c1cbf9f32f5634e4cd3f89b57d797855
bkraft <bob.kraft@infiniteleap.net>
2018-03-14 17:35:57 -0400

    Removed all scripts.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- studies/hfpef/aliases.sh
+++ studies/hfpef/aliases.sh
@@ -2,9 +2,6 @@
 
 # Aliases
 alias rehfpef='source $HFPEF_SCRIPTS_PATH/aliases.sh'
-alias hfpef_help='firefox https://github.com/theimagingcollective/nipype_workflows/wiki/HFPEF-Image-Processing &'
-
-
 
 #alias hfpef_bids_clean='$HFPEF_SCRIPTS_PATH/hfpef_bids_clean.sh'
 alias hfpef_bids_clean='$HFPEF_SCRIPTS_PATH/hfpef_clean_bids_2b.sh'
@@ -20,14 +17,3 @@ alias hfpef_gi_netprep_mbepi='$HFPEF_SCRIPTS_PATH/hfpef_gi_netprep_mbepi.sh'
 
 alias hfpef_gi_netprep='$HFPEF_SCRIPTS_PATH/hfpef_gi_netprep.sh'
 alias hfpef_netprep='/gandg/tic/nipype_workflows/netprep.py'
-
-alias cdh='cd $HFPEF_PATH; lsreport_function'
-alias cdhb='cd $HFPEF_BIDS_PATH; lsreport_function'
-alias cdhin='cd $HFPEF_PATH/incoming; lsreport_function'
-alias cdhia='cd $HFPEF_IMAGE_ANALYSIS_PATH; lsreport_function'
-alias cdhip='cd $HFPEF_IMAGE_PROCESSING_PATH; lsreport_function'
-alias cdhipl='cd $HFPEF_IMAGE_PROCESSING_PATH/fmriprep_logs; lsreport_function'
-alias cdhqc='cd $HFPEF_MRIQC_PATH; lsreport_function'
-alias cdhs='cd $HFPEF_SCRIPTS_PATH; lsreport_function'
-
-alias as_hfpef='echo previous active_study : $ACTIVE_STUDY; source $HFPEF_SCRIPTS_PATH/hfpef_set_to_active_study.sh; echo current active_study  : $ACTIVE_STUDY'
--- studies/synergy/scripts/synergy_protocol.py
+++ studies/synergy/scripts/synergy_protocol.py
@@ -1,160 +1,261 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys
-sys.path.append('/cenc/software/heudiconv/python/heudiconv/heuristics')
-
-import os
-import numpy
-from collections import namedtuple, OrderedDict
-import re
-import pprint
-
-from hdc_protocol import *
-
-
 """
-    ./heudiconv_add_header.py output/.heudiconv/syn054/ses-1/info/dicominfo_ses-1.tsv
-    csvcut -c 3,7,8,9,10,11,12,13,14,15 dicominfo.csv | csvlook --max-rows 20
+heudiconv template protocol file. 
 
-    Here is the criteria that you can use to select which dicom images to convert to NIFTI.
-
-    regex criteria are fields that accept any valid regular expression.  If you provide a simple string the
-    regex condition will return True if the string is contained in the corresponding DICOM field.  A useful
-    regex expression when searching the series_id field is '^N'  where the ^ indicates that the expression 
-    following the ^ starts at the beginning of the string.  This is helpful for selecting DICOM images
-    to convert by series number.  
-
-    range criteria are reserved for fields that contain either integers or floating point numbers.  A
-    range criteria takes a minimum and maximum value.  The numbers are inclusive so the expression
-
-    dim1 = [240, 272]   is equal to    240 <= dim1 <= 272
-
-    boolean criteria are reserved for fields that can either be True or False. 
-
-    You do not have to specify all the criteria. You just need to provide enough criteria to uniquely 
-    identify the DICOM images that you want to convert.  If your images are not specific enough and 
-    multiple images are found to be converted a number will be appended onto the name of the NIFTI images. 
-    For example,  t1w_1.nii.gz, t1w_2.nii.gz and t1w_3.nii.gz.
-   
-    Here are the most useful criteria for specifying which DICOM images to convert to NIFTI
-   
-         3. series_id (regex) - contains series number and series description
-
-         7. dim1 (range) - number of pixels in x direction
-         8. dim1 (range) - number of pixels in y direction
-         9. dim1 (range) - number of slices
-        10. dim1 (range) - number of volumes
-
-        11. TR,  (range) - Repetition Time
-        12. TE,  (range) - Echo Time
-
-        13. protocol_name (regex) - 
-
-        14. is_motion_corrected (boolean) - 
-        15. is_derived (boolean) - 
-
-        20. sequence_name (regex) - sequence name used to acquired images.  Some of Siemens sequence names contain a 
-                                    * in the name. * has special meaning in regex. It is easier to not include 
-                                    the * in regex unless you are using it as a regular expression.  If you want to
-                                    include the * in the regular expression as just another character
-                                    without any special meaning then precede it with a \
-
-        Rarely needed or used
-
-         1. total_files_till_now
-         2. example_dcm_file
-
-         4. unspecified1 (regex)
-         5. unspecified2 (regex)
-         6. unspecified3 (regex)
-
-        16. patient_id (regex) - 
-        17. study_description (regex)
-        18. referring_physician_name,
-        19. series_description (regex)
-
-        21. image_type (regex)
-        22. accession_number (regex)
-        23 patient_age (regex)
-        24. patient_sex (regex)
-        25. date (regex)
-
-   In additon to specifying the criteria for converting DICOM fields if you are converting to the BIDS 
-   directory structure you must specify the key
-
-         key - key specifies where NIFTI files will be stored in BIDS directory
-               'sub-{subject}/{session}/anat/sub-{subject}_{session}_T1w'
-
-   {subject} is the subject acrostic. This is a mandatory field and must be included
-   {session} is the session identifier and is optional.    
+This template file can be used as an example and starting point for creating your own protocol files.  
 
 
 """
 
-"""
-3-T1_MPRAGE_GRAPPA2,256,240,192,1,2.3,2.98,T1_MPRAGE_GRAPPA2,False,False
 
-4-TOF_3D_multi-slab SHORT,512,464,72,1,0.021,3.43,TOF_3D_multi-slab SHORT,False,False
-5-TOF_3D_multi-slab SHORT,184,512,1,1,0.021,3.43,TOF_3D_multi-slab SHORT,False,False
-6-TOF_3D_multi-slab SHORT,184,464,1,1,0.021,3.43,TOF_3D_multi-slab SHORT,False,False
-7-TOF_3D_multi-slab SHORT,512,464,1,1,0.021,3.43,TOF_3D_multi-slab SHORT,False,False
+def create_key(template, outtype=('nii.gz',), annotation_classes=None):
+    """
+    Create key for HDC DICOM to NIFTI conversion
 
-8-BOLD_resting 4X4X4 A>>P,64,64,35,190,2.0,25.0,BOLD_resting 4X4X4 A>>P,False,False
+    DO NOT MODIFY
 
-9-TOF_3D_multi-slab SHORT,184,464,1,1,0.021,3.43,TOF_3D_multi-slab SHORT,False,False
+    :param template:
+    :param outtype:
+    :param annotation_classes:
+    :return:
+    """
 
-10-rest_topup_A>>P,64,64,140,1,2.0,25.0,rest_topup_A>>P,False,False
-11-rest_topup_P>>A,64,64,140,1,2.0,25.0,rest_topup_P>>A,False,False
-
-12-TOF_3D_multi-slab SHORT,512,464,1,1,0.021,3.43,TOF_3D_multi-slab SHORT,False,False
-13-TOF_3D_multi-slab SHORT,512,464,1,1,0.021,3.43,TOF_3D_multi-slab SHORT,False,False
-
-14-Brain 2D PhCon BILATERAL INT. CAROTIDS,192,144,20,1,0.03972,3.86,Brain 2D PhCon BILATERAL INT. CAROTIDS,False,False
-15-Brain 2D PhCon BILATERAL INT. CAROTIDS,192,144,20,1,0.03972,3.86,Brain 2D PhCon BILATERAL INT. CAROTIDS,False,True
-
-16-Brain 2D PhCon RT MCA,192,144,20,1,0.03972,3.86,Brain 2D PhCon RT MCA,False,False
-17-Brain 2D PhCon RT MCA,192,144,20,1,0.03972,3.86,Brain 2D PhCon RT MCA,False,True
-18-Brain 2D PhCon LT MCA,192,144,20,1,0.03972,3.86,Brain 2D PhCon LT MCA,False,False
-19-Brain 2D PhCon LT MCA,192,144,20,1,0.03972,3.86,Brain 2D PhCon LT MCA,False,True
-"""
+    if template is None or not template:
+        raise ValueError('Template must be a valid format string')
+    return template, outtype, annotation_classes
 
 
+def infotodict(seqinfo):
+    """Heuristic evaluator for determining which runs belong where
 
-"""
-BIDS Templates: 
+    allowed template fields - follow python string module:
 
-    anat/
-         sub-<participant_label>
-            [_ses-<session_label>]
-            [_acq-<label>]
-            [_ce-<label>]
-            [_rec-<label>]
-            [_run-<index>]
-            [_mod-<label>]_<modality_label>.nii[.gz]
-"""
-
-def protocol():
-
-    criteria = define_sequence_criteria()
-    sequences = OrderedDict()
-
-    sequences['t1w'] = criteria(key='sub-{subject}/{session}/anat/sub-{subject}_{session}_T1w.{item:01d}',
-                                series_description='MPRAGE',  
-                                )
-
-    sequences['rest_ap'] = criteria(key='sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_acq-epi_bold.{item:01d}',
-                                series_description='BOLD_resting',  
-                                )
-
-    sequences['rest_topup_ap'] = criteria(key='sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-topup_dir-ap_epi.{item:01d}',
-                                series_description='rest_topup_A>>P',  
-                                )
-
-    sequences['rest_topup_pa'] = criteria(key='sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-topup_dir-pa_epi.{item:01d}',
-                                series_description='rest_topup_P>>A',  
-                                )
-
-    return sequences
+    item: index within category
+    subject: participant id
+    seqitem: run number during scanning
+    subindex: sub index within group
 
 
+    The namedtuple `s` contains the following fields:
+
+    * total_files_till_now
+    * example_dcm_file
+    * series_id
+    * dcm_dir_name
+    * unspecified2
+    * unspecified3
+    * dim1
+    * dim2
+    * dim3
+    * dim4
+    * TR
+    * TE
+    * protocol_name
+    * is_motion_corrected
+    * is_derived
+    * patient_id
+    * study_description
+    * referring_physician_name
+    * series_description
+    * image_type
+
+    Below is an example of the output from the iMove study
+
+    ->> hdc_look.py -s syn239 -ss 1
+
+            series_number  sequence_name                        series_description  dim1  dim2  dim3  dim4       TR     TE  is_derived  is_motion_corrected
+         0              2   *tfl3d1_16ns                         T1_MPRAGE_GRAPPA2   256   240   192     1  2.30000   2.98       False                False
+         1              3    *fl3d1r_t70                   TOF_3D_multi-slab SHORT   512   464    72     1  0.02100   3.43       False                False
+         2              4    *fl3d1r_t70           TOF_3D_multi-slab SHORT_MIP_SAG   184   512     1     1  0.02100   3.43       False                False
+         3              5    *fl3d1r_t70           TOF_3D_multi-slab SHORT_MIP_COR   184   464     1     1  0.02100   3.43       False                False
+         4              6    *fl3d1r_t70           TOF_3D_multi-slab SHORT_MIP_TRA   512   464     1     1  0.02100   3.43       False                False
+         5              7    *fl3d1r_t70                       BILAT CAROTID SLICE   184   464     1     1  0.02100   3.43       False                False
+         6              8   *epfid2d1_64                   BOLD_resting 4X4X4 A>>P    64    64    35   190  2.00000  25.00       False                False
+         7              9    *fl3d1r_t70                              RT MCA SLICE   512   464     1     1  0.02100   3.43       False                False
+         8             10    *fl3d1r_t70                              LT MCA SLICE   512   464     1     1  0.02100   3.43       False                False
+         9             11    *epse2d1_64                           rest_topup_A>>P    64    64   140     1  2.00000  25.00       False                False
+         10            12    *epse2d1_64                           rest_topup_P>>A    64    64   140     1  2.00000  25.00       False                False
+         11            13       *fl2d1r3    Brain 2D PhCon BILATERAL INT. CAROTIDS   192   144    20     1  0.03972   3.86       False                False
+         12            14  *fl2d1_v100in  Brain 2D PhCon BILATERAL INT. CAROTIDS_P   192   144    20     1  0.03972   3.86        True                False
+         13            15       *fl2d1r3                     Brain 2D PhCon RT MCA   192   144    20     1  0.03972   3.86       False                False
+         14            16  *fl2d1_v100in                   Brain 2D PhCon RT MCA_P   192   144    20     1  0.03972   3.86        True                False
+         15            17       *fl2d1r3                     Brain 2D PhCon LT MCA   192   144    20     1  0.03972   3.86       False                False
+         16            18  *fl2d1_v100in                   Brain 2D PhCon LT MCA_P   192   144    20     1  0.03972   3.86        True                False
+
+        ... cardiac scans are acquired after this point.
+
+
+    key-value tags
+    --------------
+
+    BIDS uses a key-value pairs in the naming convention.  The sub-<participant_label> is a required field. It is used
+    to create a subject specific directory in the BIDS directory.
+
+    It must also be included in each and every filename. The ses-<session_label> is a bug the <session_label> includes
+    the key.  This means that {session} translates to
+    ses-<session-label>.
+
+    Only certain keys are associated with each imaging modality.
+    key-value pairs are in [] are optional.  If a key-value pair is not in [] then it must be included in the name.
+    For example every filename must include sub-<participant_label> to be BIDS compliant.  The session key-value
+    is optional. However, if you include session key-value in the path then you must also include it in the
+    filename.
+
+    Another example of required and optional key-value pairs is in the FUNC images.  Here is the key-value
+    pairs taken from the BIDS documentation for images in the func directory.
+
+     _task-<task_label>[_acq-<label>][_rec-<label>][_run-<index>][_echo-<index>]_bold
+
+     You can see that task-<task_label> is not in [] so it is required. While _acq-<acq_label> is in [] and is
+     therefore optional.
+
+     Below I have included some notes for the key-value pairs for the different imaging modalities and BIDS
+     sub-directories.
+
+
+    common key-value tags:
+
+        sub-<participant_label>/[ses-<session_label>/]/<dir>/sub-<participant_label>[_ses-<session_label>]
+
+        <dir> = anat, func, fmap, dwi, swi
+
+
+
+    anat key-value tags:
+
+        [_acq-<label>][_ce-<label>][_rec-<label>][_run-<index>]<modality_label>.nii[.gz]
+        [_acq-<label>][_ce-<label>][_rec-<label>][_run-<index>][_mod-<label>]_defacemask.nii[.gz]
+
+
+        Imaging Type/Name,       modality_label
+        T1 weighted              T1w
+        T2 weighted              T2w
+        T1 Rho map               T1rho
+        T1 map                   T1map
+        T2 map                   T2map
+        T2*                      T2star
+        FLAIR                    FLAIR
+        FLASH                    FLASH
+        Proton density           PD
+        Proton density map       PDmap
+        Combined PD/T2           PDT2
+        Inplane T1               inplaneT1
+        Inplane T2               inplaneT2
+        Angiography              angio
+
+
+
+    func/ _task-<task_label>[_acq-<label>][_rec-<label>][_run-<index>][_echo-<index>]_bold
+
+    fmap/  [_acq-<label>][_run-<run_index>]_phasediff
+           [_acq-<label>][_run-<run_index>]_magnitude1
+
+           [_acq-<label>]_dir-<dir_label>[_run-<run_index>]_epi
+
+    dwi/   [_acq-<label>][_run-<index>]_dwi
+           [_acq-<label>][_run-<index>]_sbref
+
+    swi/  [_acq-<label>][_rec-<label>]_part-<phase|mag>[_coil-<index>][_echo-<index>][_run-<index>]_GRE.nii[.gz]
+
+        https://docs.google.com/document/d/1kyw9mGgacNqeMbp4xZet3RnDhcMmf4_BmRgKaOkO2Sc/edit
+
+
+    asl/  _task-<task_label>[_acq-<label>][_rec-<label>][_run-<index>]_asl.nii[.gz]
+
+        https://docs.google.com/document/d/15tnn5F10KpgHypaQJNNGiNKsni9035GtDqJzWqkkP6c/edit#heading=h.mqkmyp254xh6
+
+    """
+
+    # Create a key for each DICOM file to convert.  The key tells HDC where and how to name the converted DICOM file.
+    # The naming convention is very strict. The rules are established by BIDS  http://bids.neuroimaging.io/
+    #
+    #  anat, func, fmap are established directories where the data can reside.
+    #
+    #  {subject} is the variable which will be replaced by the subject's acrostic
+    #
+    #  {session} is the variable which will be replaced by the session value key.  {session} is inconsistent
+    #            and is replaced by the key and it's value. An example, should help clarify this inconsistency
+    #            {session} is replaced by ses-{session_id}.
+    #
+    #  {acq} is the acquisition value
+    #
+    #  {item:01d} is the item counter padded by two zeros.  This allows HDC to create a unique name if it finds
+    #             multiple images that meet your criteria.  We recommend that only one DICOM image should match your
+    #             criteria for data conversion. However, this can not happen if the scan is repeated.
+    #
+
+    t1 = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_T1w.{item:01d}')
+
+    tof = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-3dtof_angio.{item:01d}')
+
+    pc_bilateral = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-pcbilateral_angio.{item:01d}')
+    pc_right_mca = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-pcright_angio.{item:01d}')
+    pc_left_mca = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-pcleft_angio.{item:01d}')
+
+    rest_ap = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_acq-epi_bold.{item:01d}')
+    rest_topup_ap = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-topup_dir-ap_epi.{item:01d}')
+    rest_topup_pa = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-topup_dir-pa_epi.{item:01d}')
+
+    # Create an empty dictionary called info for each key
+
+    info = {t1: [],
+            tof: [],
+            pc_bilateral: [],
+            pc_right_mca: [],
+            pc_left_mca: [],
+            rest_ap: [],
+            rest_topup_ap: [],
+            rest_topup_pa: [],
+            }
+
+    # Loop over each sequence. Use if statements to determine which sequences should be linked to which key
+
+    for idx, s in enumerate(seqinfo):
+
+        # --------------------------------------
+        # anat - T1
+
+        if 'MPRAGE' in s.series_description:
+                info[t1].append([s.series_id])
+
+        if (('TOF_3D' in s.series_description) and
+                ('fl3d1r_t70' in s.sequence_name) and
+                (s.dim3 >= 64) and (s.dim3 <= 128)):
+            info[tof].append([s.series_id])
+
+        if (('Brain 2D PhCon BILATERAL' in s.series_description) and
+                ('fl2d1r3' in s.sequence_name) and
+                (not s.is_derived)):
+                info[pc_bilateral].append([s.series_id])
+
+        if (('Brain 2D PhCon RT MCA' in s.series_description) and
+                ('fl2d1r3' in s.sequence_name) and
+                (not s.is_derived)):
+                info[pc_right_mca].append([s.series_id])
+
+        if (('Brain 2D PhCon LT MCA' in s.series_description) and
+                ('fl2d1r3' in s.sequence_name) and
+                (not s.is_derived)):
+                info[pc_left_mca].append([s.series_id])
+
+        # --------------------------------------
+        # resting state bold
+
+        if (('BOLD_resting 4X4X4 A>>P' in s.series_description) and
+                ('epfid2d1_64' in s.sequence_name) and
+                (s.dim3 == 35) and
+                (s.dim4 == 190)):
+                info[rest_ap].append([s.series_id])
+
+        if (('rest_topup_A>>P' in s.series_description) and
+                ('epse2d1_64' in s.sequence_name) and
+                (s.dim3 == 140) and
+                (s.dim4 == 1)):
+                info[rest_topup_ap].append([s.series_id])
+
+        if (('rest_topup_P>>A' in s.series_description) and
+                ('epse2d1_64' in s.sequence_name) and
+                (s.dim3 == 140) and
+                (s.dim4 == 1)):
+                info[rest_topup_pa].append([s.series_id])
+
+    return info
