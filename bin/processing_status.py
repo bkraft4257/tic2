c9f9aebe7db7cab3a6e5dc800c0e7ab16f8d8971
bkraft <bob.kraft@infiniteleap.net>
2018-03-15 18:19:12 -0400

    Wrote new function to create acrostic.csv
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- bin/processing_status.py
+++ bin/processing_status.py
@@ -154,8 +154,8 @@ def main():
                     .fillna(False)
                     )
 
-    for ii in df_full_list.iterrows():
-        print(ii) # re.sub("\s+", ",", df_full_list.to_string().strip()))
+    for row in df_full_list.iterrows():
+        print(row.to_frame().T) # re.sub("\s+", ",", df_full_list.to_string().strip()))
 
     # display(df_full_list.pipe(_clean_nan, nan_option=in_args.nan),
     #         subject_only=in_args.subject_only,
