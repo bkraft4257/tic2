036c9a68113ebeeb0f2b90844775003058840656
bkraft <bob.kraft@infiniteleap.net>
2018-03-19 12:10:52 -0400

    Refactored plot_overlay.py so it is in the bin directory as a command line function.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- bin/clean_bids.sh
+++ bin/clean_bids.sh
@@ -6,6 +6,15 @@
 # This is a simple pass through script and will run whatever script the ACTIVE_CLEAN_BIDS environment variable
 # points to.
 
+echo
+echo
+echo 'active study = ' $ACTIVE_STUDY
+echo 'bids path    = ' $ACTIVE_BIDS_PATH
+echo 'clean_bids   = ' $ACTIVE_CLEAN_BIDS
+echo
+echo 'parameters   = ' $@
+echo
+echo
 
 $ACTIVE_CLEAN_BIDS $@
 
