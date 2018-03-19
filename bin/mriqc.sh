ba787df9c6f96c00a1c1f3742bbacb3e52bff277
bkraft <bob.kraft@infiniteleap.net>
2018-03-19 15:20:21 -0400

    Updated mriqc
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 07ad7788397e9a0a8de1ad0778815c3a8b92bdb8
    Former-commit-id: 61ca10080d97d234ffa13cad1a8e76884a02701c

--- bin/mriqc.sh
+++ bin/mriqc.sh
@@ -15,18 +15,20 @@ study_prefix=$(echo "${ACTIVE_STUDY,,}")
 datetime_stamp=`date '+d%Y%m%d_%H:%M:%S'`
 log_file=${ACTIVE_IMAGE_PROCESSING_LOG_PATH}/${study_prefix}_${app}_${datetime_stamp}.log
 
-echo ' ' | tee log_file
-echo 'datetime.now()         = ' $(date) | tee log_file
-echo 'active study           = ' $ACTIVE_STUDY | tee log_file
-echo 'bids app               = ' $BIDS_APP | tee log_file
-echo 'bids path              = ' $ACTIVE_BIDS_PATH | tee log_file
-echo 'log path               = ' $ACTIVE_IMAGE_PROCESSING_LOG_PATH | tee log_file
-echo 'output path            = ' $ACTIVE_APP_OUTPUT_PATH | tee log_file
-echo 'working path           = ' $ACTIVE_APP_WORKING_PATH | tee log_file
-echo 'SUBJECTS_DIR           = ' $SUBJECTS_DIR | tee log_file
-echo 'app singularity image  = ' $APP_SINGULARITY_IMAGE | tee log_file
-echo 'log file               = ' $log_file | tee log_file
-echo ' ' | tee log_file
+source $TIC_PATH/studies/active/scripts/bids_app_status.sh
+
+#echo ' ' | tee log_file
+#echo 'datetime.now()         = ' $(date) | tee log_file
+#echo 'active study           = ' $ACTIVE_STUDY | tee log_file
+#echo 'bids app               = ' $BIDS_APP | tee log_file
+#echo 'bids path              = ' $ACTIVE_BIDS_PATH | tee log_file
+#echo 'log path               = ' $ACTIVE_IMAGE_PROCESSING_LOG_PATH | tee log_file
+#echo 'output path            = ' $ACTIVE_APP_OUTPUT_PATH | tee log_file
+#echo 'working path           = ' $ACTIVE_APP_WORKING_PATH | tee log_file
+#echo 'SUBJECTS_DIR           = ' $SUBJECTS_DIR | tee log_file
+#echo 'app singularity image  = ' $APP_SINGULARITY_IMAGE | tee log_file
+#echo 'log file               = ' $log_file | tee log_file
+#echo ' ' | tee log_file
 
 
 # NOTE: any -B mount points must exist in the container
