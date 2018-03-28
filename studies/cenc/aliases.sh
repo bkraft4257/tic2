0bd2417cfea132fccc151aead35bc2ff553ebf79
bkraft <bob.kraft@infiniteleap.net>
2018-03-28 11:47:38 -0400

    Adding CENC folder

---
+++ studies/cenc/aliases.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Aliases
+alias recenc='source $CENC_SCRIPTS_PATH/../aliases.sh'
+
+# When creating this alias you will need to update the study_switcher.py to include the cenc
+# name.
+alias sw_cenc='study_switcher.py -s cenc; source $TIC_INIT_PATH/tic_study_switcher.sh'
+
+
