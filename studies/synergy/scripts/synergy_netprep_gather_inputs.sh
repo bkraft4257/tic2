7a873dc0007bc9996c3282fa0fd9b4d1e510fd9a
bkraft <bob.kraft@infiniteleap.net>
2018-04-11 14:19:59 -0400

    Changed synergy filename in script to match file name changes.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 80526925d1dcc438bbb5b825e98328e97d539029
    Former-commit-id: a26b4a647121b76c0fd75bc168f8b05451dbdcff

--- studies/synergy/scripts/synergy_netprep_gather_inputs.sh
+++ studies/synergy/scripts/synergy_netprep_gather_inputs.sh
@@ -12,16 +12,24 @@ session_id=${2-1}
 subject=sub-${subject_id}
 session=ses-${session_id}
 
-confound_columns=1,2,13,14,15,16,17,18,24,25,26,27,28,29
-
 netprep_input=${SYNERGY_NETPREP_PATH}/${subject}/${session}/input
+
+bold_mni_preproc=$SYNERGY_FMRIPREP_PATH/${subject}/${session}/func/${subject}_${session}_task-rest_acq-epi_bold_space-MNI152NLin2009cAsym_preproc.nii.gz
+bold_confounds_tsv=$SYNERGY_FMRIPREP_PATH/${subject}/${session}/func/${subject}_${session}_task-rest_acq-epi_bold_confounds.tsv
+t1w_gm_probtissue=$SYNERGY_FMRIPREP_PATH/${subject}/${session}/anat/${subject}_${session}_T1w_class-GM_probtissue.nii.gz
+
+grep_results=$(grep NonSteadyStateOutlier00 $bold_confounds_tsv)
+
+if [[ -z $grep_results ]]; then
+    confound_name_columns="WhiteMatter,GlobalSignal,aCompCor00,aCompCor02,aCompCor03,aCompCor04,aCompCor05,X,Y,Z,RotX,RotY,RotZ"
+else
+    confound_name_columns="WhiteMatter,GlobalSignal,aCompCor00,aCompCor02,aCompCor03,aCompCor04,aCompCor05,X,Y,Z,RotX,RotY,RotZ,NonSteadyStateOutlier00"
+fi
+
 
 mkdir -p ${netprep_input}
 cp ${SYNERGY_SCRIPTS_PATH}/synergy_netprep.yaml ${netprep_input}/netprep.yaml
 
-bold_mni_preproc=$SYNERGY_FMRIPREP_PATH/${subject}/${session}/func/${subject}_${session}_task-rest_acq-epi_bold_space-MNI152NLin2009cAsym_preproc.nii.gz
-t1w_gm_probtissue=$SYNERGY_FMRIPREP_PATH/${subject}/${session}/anat/${subject}_${session}_T1w_class-GM_probtissue.nii.gz
-bold_confounds_tsv=$SYNERGY_FMRIPREP_PATH/${subject}/${session}/func/${subject}_${session}_task-rest_acq-epi_bold_confounds.tsv
 
 ln -f ${bold_mni_preproc} ${netprep_input}/bold_mni_preproc.nii.gz
 ln -f ${t1w_gm_probtissue} ${netprep_input}/t1w_gm_probtissue.nii.gz
@@ -34,15 +42,16 @@ echo ${confound_name_columns}
 echo
 
 
-csvcut -t -c ${confound_columns} ${bold_confounds_tsv} > ${netprep_input}/bold_confound.csv
+csvcut -t -c ${confound_name_columns} ${bold_confounds_tsv} > ${netprep_input}/bold_confound.csv
 
 
 
 cd ${netprep_input}
-echo 
+echo
 pwd
 echo
 ls
 echo
+
 
 
