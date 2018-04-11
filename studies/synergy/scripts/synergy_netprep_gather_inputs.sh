22a3275ad3f5fae5662ffced4e8d572f1bca9d24
bkraft <bob.kraft@infiniteleap.net>
2018-04-11 14:06:05 -0400

    Changed synergy filename in script to match file name changes.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- studies/synergy/scripts/synergy_netprep_gather_inputs.sh
+++ studies/synergy/scripts/synergy_netprep_gather_inputs.sh
@@ -10,7 +10,7 @@ confound_columns=1,2,13,14,15,16,17,18,24,25,26,27,28,29
 netprep_input=${SYNERGY_NETPREP_PATH}/${subject}/${session}/input
 
 mkdir -p ${netprep_input}
-cp ${SYNERGY_SCRIPTS_PATH}/syn_netprep.yaml ${netprep_input}/syn_netprep.yaml
+cp ${SYNERGY_SCRIPTS_PATH}/syn_netprep.yaml ${netprep_input}/synergy_netprep.yaml
 
 bold_mni_preproc=$SYNERGY_FMRIPREP_PATH/${subject}/${session}/func/${subject}_${session}_task-rest_acq-epi_bold_space-MNI152NLin2009cAsym_preproc.nii.gz
 t1w_gm_probtissue=$SYNERGY_FMRIPREP_PATH/${subject}/${session}/anat/${subject}_${session}_T1w_class-GM_probtissue.nii.gz
