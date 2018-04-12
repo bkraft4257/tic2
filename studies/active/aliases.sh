a9bbd24cbaa5e42e9f9f7dafd6e30785ad881f04
bkraft <bob.kraft@infiniteleap.net>
2018-04-12 13:50:43 -0400

    Added operations for cleaning BIDS key/value pairs
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- studies/active/aliases.sh
+++ studies/active/aliases.sh
@@ -24,3 +24,21 @@ alias sw='study_switcher.sh'
 export ACTIVE_HEUDICONV_PATTERN="{subject}/2*/*/*.DCM"
 
 
+function myfunc()
+{
+    if study_switcher.py $@; then
+
+
+        params=$@
+
+        echo "$params" | grep -q "\-d"
+
+        if [ $? -ne 0 ];then
+            source $TIC_INIT_PATH/tic_study_switcher.sh
+
+            echo
+           echo '    ACTIVE_STUDY is now' $ACTIVE_STUDY
+            echo
+        fi
+    fi
+}
