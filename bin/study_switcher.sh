5700a8a2aafeb80c82f9eb348023d1c9c0d4de42
bkraft <bob.kraft@infiniteleap.net>
2018-04-12 13:27:29 -0400

    Added operations for cleaning BIDS key/value pairs
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- bin/study_switcher.sh
+++ bin/study_switcher.sh
@@ -9,5 +9,6 @@ echo "$params" | grep -q "\-d"
 
 if [ $? -ne 0 ];then
     source $TIC_INIT_PATH/tic_study_switcher.sh
-    echo 'ACTIVE_STUDY is ' $ACTIVE_STUDY
+    echo '\n\tACTIVE_STUDY is now' $ACTIVE_STUDY
+    echo
 fi
