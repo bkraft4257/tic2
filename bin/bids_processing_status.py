e27d03d341506dca337e700c4e7a8666d70cd068
bkraft <bob.kraft@infiniteleap.net>
2018-04-12 18:21:46 -0400

    Added ability to ignore session during search of bps.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: a252ca15a3ffb6a9e0b66ede66e2d2d2d50a0397
    Former-commit-id: affd8bfd28c1aa78d5dab874fbeed0b7aeb0226b

--- bin/bids_processing_status.py
+++ bin/bids_processing_status.py
@@ -274,7 +274,11 @@ def main():
         df_files_2.columns = [f'ses-{x+1}_processed' for x in range(len(df_files_2.columns))]
 
     except ValueError:
-        print(f'\n{Fore.RED}Unable to stack. {Fore.WHITE} More than one file found per subject per session.∆\n')
+
+        if session_key_value is None:
+            print(f'\n{Fore.RED}Unable to stack. {Fore.WHITE} More than one file found per subject.\n')
+        else:
+            print(f'\n{Fore.RED}Unable to stack. {Fore.WHITE} More than one file found per subject per session.\n')
 
         with pandas.option_context('display.max_rows', in_args.display_max_rows):
             print(df_files)
