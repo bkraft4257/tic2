1fedfe82034554f0eb4b17a41889e375346a1026
bkraft <bob.kraft@infiniteleap.net>
2018-04-12 17:11:08 -0400

    Updated create_acrostic_list.py. Renamed to acrostic_list.py
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 01eddc5fd0bd1c6ce562165f82263cd75b508876
    Former-commit-id: f81ee1b65d46df680b5b09a516f3428938c84722

--- bin/ls_participants.sh
+++ bin/ls_participants.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 echo
-ls $ACTIVE_BIDS_PATH/participants*.tsv
+echo "List of participants*.tsv"
+echo "--------------------------------------------------------------------------"
+ls -1 $ACTIVE_BIDS_PATH/participants*.tsv
+echo
 echo
 
+echo "cat participants_all.tsv"
+echo "--------------------------------------------------------------------------"
 cat $ACTIVE_BIDS_PATH/participants_all.tsv
+echo
