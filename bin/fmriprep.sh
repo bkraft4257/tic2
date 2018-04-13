fc7f27ef6c718a27c98b2dd748c6d5322950085b
bkraft <bob.kraft@infiniteleap.net>
2018-04-13 18:11:54 -0400

    Draft of mcf_protcol
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 56b36b9c29bab6c880d8215f77604a989dc2ae12
    Former-commit-id: 0722a6a45acaea4e1c172654ecb70d79c9e661ee

--- bin/fmriprep.sh
+++ bin/fmriprep.sh
@@ -43,7 +43,7 @@ nohup time /usr/local/bin/singularity run -w -B /cenc -B /gandg -B /bkraft1 \
                  $ACTIVE_BIDS_PATH \
                  $ACTIVE_APP_OUTPUT_PATH \
                  --work-dir $ACTIVE_APP_WORKING_PATH \
-                 participant ${@} >> $log_file 2>&1 &
+                 participant ${parameters} >> $log_file 2>&1 &
 
 echo "Waiting 30 seconds before displaying the log file ..."
 sleep 30
