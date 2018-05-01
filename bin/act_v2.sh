66ada4335966e763d9aa16d51cfa54b2fa2abcb1
bkraft <bob.kraft@infiniteleap.net>
2018-05-01 09:24:43 -0400

    Updated ACT
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- bin/act_v2.sh
+++ bin/act_v2.sh
@@ -42,8 +42,9 @@ nohup time /usr/local/bin/singularity run -w -B /cenc -B /gandg -B /bkraft1 \
                  $APP_SINGULARITY_IMAGE \
                  $ACTIVE_BIDS_PATH \
                  $ACTIVE_APP_OUTPUT_PATH \
-                 --work-dir $ACTIVE_APP_WORKING_PATH \
-                 participant ${parameters} >> $log_file 2>&1 &
+                 participant \
+                 -v --work-dir $ACTIVE_APP_WORKING_PATH \
+                  ${parameters} >> $log_file 2>&1 &
 
 echo "Waiting 30 seconds before displaying the log file ..."
 sleep 30
