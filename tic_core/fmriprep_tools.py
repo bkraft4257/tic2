9a2143f7e853913bc90fecac55ad6d1ea26d4edd
bkraft <bob.kraft@infiniteleap.net>
2018-05-11 14:43:54 -0400

    Updated fmap_intended_for.py to include echo_times and work for a single file.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 3cb8ebcc2df66915c35b4b756117b63f870a466f
    Former-commit-id: 2af2d2c7d2148f728b1f5c0551533b991a27ca18

--- tic_core/fmriprep_tools.py
+++ tic_core/fmriprep_tools.py
@@ -163,13 +163,3 @@ def print_echo_times_from_list(echo_times):
         for ii, jj in enumerate(echo_times):
             print(f'\n\n  "EchoTime{ii}": "{jj}",')
         print('\n')
-
-
-def print_intended_for_from_list(files):
-
-    print(f'\n\n  "IntendedFor": ["{files[0]}",')
-
-    for ii_file in files[1:-1]:
-        print(f'                  "{ii_file}",')
-
-    print(f'                  "{files[-1]}"],\n')
