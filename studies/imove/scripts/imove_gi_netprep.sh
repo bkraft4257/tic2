7a24930ea91a292adccac873f12d9a7ee3650464
bkraft <bob.kraft@infiniteleap.net>
2018-05-18 12:57:51 -0400

    Updated IMOVE netprep
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 8739e36998aa4ebfe3320cebacf2ca5bd528c34c
    Former-commit-id: d84eb7772db04fccdd20c73ffd92c4e2f0dbbed4

---
+++ studies/imove/scripts/imove_gi_netprep.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# imove_gi_netprep.sh gathers the inputs for running netprep.
+
+
+if [[ $# -lt 1 ]]; then
+   echo "Usage: hfpef_gi_netprep.sh <subject_value,hfs070> <session_value,1>"
+   echo "You may only enter one subject value and session value at a time."
+
+   exit 0
+fi
+
+subject_id=${1}
+session_id=${2-1}
+subject=sub-${subject_id}
+session=ses-${session_id}
+
+
+echo
+echo "hfpef_gi_netprep_epi.sh =============================================================================="
+echo
+
+$IMOVE_SCRIPTS_PATH/imove_gi_netprep_epi.sh $subject_id  $session_id
+
+
+
+echo
+echo "hfpef_gi_netprep_mbepi.sh =============================================================================="
+echo
+
+$IMOVE_SCRIPTS_PATH/imove_gi_netprep_mbepi.sh $subject_id  $session_id
+
+
+
+
+
