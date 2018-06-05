f95224876ad1d9ffe297ba4675cedea22c3e824d
bkraft <bob.kraft@infiniteleap.net>
2018-06-05 15:33:29 -0400

    Moving clean_bids to python. Updated permissions.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 8f0643777d83b6fe9ee50a783d4dd0eed3aff96a
    Former-commit-id: bc7878e4b7ae517a4e0af7ed5b04cadfea3aacf4

--- bin/act.sh
+++ bin/act.sh
@@ -42,7 +42,7 @@ log_file=${ACTIVE_IMAGE_PROCESSING_LOG_PATH}/${study_prefix}_${BIDS_APP}_${datet
 export FULL_BIDS_APP_COMMAND="act_full_command=$SINGULARITY_COMMAND \
  $ACTIVE_SINGULARITY_USER_BIND_PATHS \
  $APP_SINGULARITY_IMAGE \
- $ACTIVE_BIDS_PATH $ACTIVE_ACT_OUTPUT_PATH \
+ $ACTIVE_BIDS_PATH $ACTIVE_APP_OUTPUT_PATH \
  participant $parameters"
 
 # Write information to log file
