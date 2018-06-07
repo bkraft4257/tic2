e64cb010e09a96ad710ec99f52216214e741d23f
bkraft <bob.kraft@infiniteleap.net>
2018-06-07 16:50:15 -0400

    Converted string to a single string to be printed.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: a27f40502a9c5983e914b96297d763b1d28db5a3
    Former-commit-id: 67cd5d5cde45da233d595c3764d596f758cf6b81

--- bin/fmap_intended_for.py
+++ bin/fmap_intended_for.py
@@ -7,8 +7,28 @@ Create a list of functional files to be included in
 
 import sys
 import argparse
+import shutil
 
 from tic_core import fmriprep_tools
+
+
+def _write_intended_for(intended_for_string, input_file, output_file=None):
+
+    if output_file is None:
+        output_file = input_file + '.new'
+
+    with open(input_file) as old, open(output_file, 'w') as new:
+
+        for ii, ii_line in enumerate(old):
+
+            if ii == 1:
+                new.write(intended_for_string)
+            else:
+                new.write(ii_line)
+
+    # shutil.move('newtest', 'test')
+
+    return
 
 
 def _argparse():
@@ -27,7 +47,9 @@ def _argparse():
                         help="Echo Times",
                         default=[0.00492, 0.00738])
 
-    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument('-i', '--input_file', help='BIDS fmap json file',  default=None)
+
+    parser.add_argument('-o', '--output_file', help='Output file', default=None)
 
     parser.add_argument("-f", "--fmap", help="Boolean flag to display EchoTime.",
                         action="store_true",
@@ -46,7 +68,12 @@ def main():
     insert_string = fmriprep_tools.print_intended_for_from_list(stripped_files)
 
     if in_args.fmap:
-        insert_string = fmriprep_tools.print_echo_times_from_list(in_args.echo_times)
+        insert_string += fmriprep_tools.print_echo_times_from_list(in_args.echo_times)
+
+    if in_args.input_file is not None:
+        _write_intended_for(insert_string,
+                            input_file=in_args.input_file,
+                            output_file=in_args.outpuf_file)
 
     print(insert_string)
 
