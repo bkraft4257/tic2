a3563264cf6d1e1b1f091cfb99f717ab8282fcc5
bkraft <bob.kraft@infiniteleap.net>
2018-06-08 09:27:01 -0400

    Added script to run intended for MFC.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 03f58cf4c9afd073259153059cf1d4df9d0c7829
    Former-commit-id: e5c4bfb063379d24326a1627c73b2da8a8b0379d

--- studies/mfc/scripts/mfc_fmap_intended_for.sh
+++ studies/mfc/scripts/mfc_fmap_intended_for.sh
@@ -6,7 +6,20 @@
 # This script does not do any error checking.  It MUST be run in the fmap directory.
 #
 
+echo
+echo
 
 find ../func/ -name "*post*topup*.nii.gz" | sort | xargs fmap_intended_for.py -i *_acq-postEpi_dir-{ap,pa}_epi.json --overwrite -v
+
+echo
+echo
+
 find ../func/ -name "*pre*topup*.nii.gz"  | sort | xargs fmap_intended_for.py -i *_acq-preEpi_dir-{ap,pa}_epi.json --overwrite -v
+
+echo
+echo
+
 find ../func/ -name "*fmap*.nii.gz"       | sort | xargs fmap_intended_for.py -i *_acq-pre_phasediff.json --overwrite -v -f
+
+echo
+echo
