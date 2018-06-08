ad5d492295990af017824dccf898e633ab83fccf
bkraft <bob.kraft@infiniteleap.net>
2018-06-08 09:24:22 -0400

    Added ability for multiple input files.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 0919114f56143ae7557240711518d7b4c35c4231
    Former-commit-id: 9186d43d211468b3127188b8fc50476b99a937fe

---
+++ studies/mfc/scripts/mfc_fmap_intended_for.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script finds the functional data sets and inserts the IntendedFor and EchoTimes (for phase difference fieldmaps)
+# into the corresponding JSON files in the fmap directory.
+#
+# This script does not do any error checking.  It MUST be run in the fmap directory.
+#
+
+
+find ../func/ -name "*post*topup*.nii.gz" | sort | xargs fmap_intended_for.py -i *_acq-postEpi_dir-{ap,pa}_epi.json --overwrite -v
+find ../func/ -name "*pre*topup*.nii.gz"  | sort | xargs fmap_intended_for.py -i *_acq-preEpi_dir-{ap,pa}_epi.json --overwrite -v
+find ../func/ -name "*fmap*.nii.gz"       | sort | xargs fmap_intended_for.py -i *_acq-pre_phasediff.json --overwrite -v -f
