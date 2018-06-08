db8297705ad6120f6b4ab590e04cfb7c7db451ff
bkraft <bob.kraft@infiniteleap.net>
2018-06-08 09:30:35 -0400

    Added script to run intended for MFC.
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>
    
    Former-commit-id: 059f2685485f88fe20d7bd64a29be0e3703c6f3e
    Former-commit-id: 2f4a8f5d9b3740714dba8fabeef675ec2fa80275

--- studies/mfc/aliases.sh
+++ studies/mfc/aliases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Aliases
-alias remfc='source $MFC_SCRIPTS_PATH/aliases.sh'
+alias remfc='source $MFC_SCRIPTS_PATH/../aliases.sh'
 alias mfc_fif='$MFC_SCRIPTS_PATH/mfc_fmap_intended_for.sh'
 
 
