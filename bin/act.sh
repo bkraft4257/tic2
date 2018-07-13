ce9cc8313bdeda9249e7aca9e5cbaebc45474f85
Craig Hamilton <crhamilt@wakehealth.edu>
2018-07-13 15:19:04 -0400

    Change "participant-label" to "participant_label" in act.sh

--- bin/act.sh
+++ bin/act.sh
@@ -11,12 +11,12 @@ ACTIVE_APP_WORKING_PATH=$ACTIVE_IMAGE_PROCESSING_PATH/_working
 # Convert to lower case
 study_prefix=$(echo "${ACTIVE_STUDY,,}")
 
-# mriqc.sh and fmriprep.sh uses --participant-label and hdc.sh uses -s to indicated the subject acrostic.
-# I am using sed as a hack to replace -s with --participant-label.  This allows people to use the shorter
+# mriqc.sh and fmriprep.sh uses --participant_label and hdc.sh uses -s to indicated the subject acrostic.
+# I am using sed as a hack to replace -s with --participant_label.  This allows people to use the shorter
 # -s.
 #
 
-parameters=$(echo $@ | sed -e 's/-s /--participant-label /')
+parameters=$(echo $@ | sed -e 's/-s /--participant_label /')
 
 # create the output and work directories parallel to BIDS hierarchy, not inside it
 
@@ -39,12 +39,12 @@ log_file=${ACTIVE_IMAGE_PROCESSING_LOG_PATH}/${study_prefix}_${BIDS_APP}_${datet
 
 
 # run it in the background so that it continues if user logs out
-export FULL_BIDS_APP_COMMAND="act_full_command=$SINGULARITY_COMMAND \
- $ACTIVE_SINGULARITY_USER_BIND_PATHS \
- $APP_SINGULARITY_IMAGE \
- $ACTIVE_BIDS_PATH \
- $ACTIVE_ACT_OUTPUT_PATH \
- participant $parameters"
+# export FULL_BIDS_APP_COMMAND="act_full_command=$SINGULARITY_COMMAND \
+#  $ACTIVE_SINGULARITY_USER_BIND_PATHS \
+#  $APP_SINGULARITY_IMAGE \
+#  $ACTIVE_BIDS_PATH \
+#  $ACTIVE_APP_OUTPUT_PATH \
+#  participant $parameters "
 
 # Write information to log file
 source $TIC_PATH/studies/active/scripts/bids_app_status.sh
@@ -57,7 +57,7 @@ nohup time $SINGULARITY_COMMAND \
            $APP_SINGULARITY_IMAGE \
            $ACTIVE_BIDS_PATH \
            $ACTIVE_APP_OUTPUT_PATH \
-           participant $parameters > $log_file 2>&1 &
+           participant $parameters >> $log_file 2>&1 &
 
 echo "Waiting 30 seconds before displaying the log file ..."
 sleep 30
