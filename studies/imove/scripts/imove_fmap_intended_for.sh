3a5a0419aedb102c47b98cdb1a413bf7694bbb78
bkraft <bob.kraft@infiniteleap.net>
2018-08-27 15:43:45 -0400

    Changed imove_trial to imove
    Signed-off-by: bkraft <bob.kraft@infiniteleap.net>

--- studies/imove/scripts/imove_fmap_intended_for.sh
+++ studies/imove/scripts/imove_fmap_intended_for.sh
@@ -5,13 +5,13 @@
 #
 # This script does not do any error checking.  It MUST be run in the fmap directory.
 #
-# sub-hfs075_ses-1_acq-bold_phasediff.json:  "IntendedFor": [ "ses-1/func/sub-hfs075_ses-1_task-rest_acq-epi_rec-fmap_bold.nii.gz" ],
-# sub-hfs075_ses-1_acq-epse_dir-ap_epi.json:  "IntendedFor": [ "ses-1/func/sub-hfs075_ses-1_task-rest_acq-epi_rec-topup_bold.nii.gz" ],
-# sub-hfs075_ses-1_acq-epse_dir-pa_epi.json:  "IntendedFor": [ "ses-1/func/sub-hfs075_ses-1_task-rest_acq-epi_rec-topup_bold.nii.gz" ],
-# sub-hfs075_ses-1_acq-mbepi_dir-lr_epi.json:  "IntendedFor": [ "ses-1/func/sub-hfs075_ses-1_task-rest_acq-mbepi_bold.nii.gz" ],
-# sub-hfs075_ses-1_acq-mbepi_dir-rl_epi.json:  "IntendedFor": [ "ses-1/func/sub-hfs075_ses-1_task-rest_acq-mbepi_bold.nii.gz" ],
-# sub-hfs075_ses-1_acq-pcasl_dir-lr_epi.json:  "IntendedFor": [ "ses-1/func/sub-hfs075_ses-1_task-rest_acq-pcasl_bold.nii.gz" ],
-# sub-hfs075_ses-1_acq-pcasl_dir-rl_epi.json:  "IntendedFor": [ "ses-1/func/sub-hfs075_ses-1_task-rest_acq-pcasl_bold.nii.gz" ],
+#                                        json_file                                              intended_for_filename  exists
+# 0   sub-imove1101_ses-1_acq-epse_dir-ap_epi.json  /func/sub-imove1101_ses-1_task-rest_acq-epi_rec-topup_bold.nii.gz    True
+# 1   sub-imove1101_ses-1_acq-epse_dir-pa_epi.json  /func/sub-imove1101_ses-1_task-rest_acq-epi_rec-topup_bold.nii.gz    True
+# 2  sub-imove1101_ses-1_acq-mbepi_dir-lr_epi.json          /func/sub-imove1101_ses-1_task-rest_acq-mbepi_bold.nii.gz    True
+# 3  sub-imove1101_ses-1_acq-mbepi_dir-rl_epi.json          /func/sub-imove1101_ses-1_task-rest_acq-mbepi_bold.nii.gz    True
+# 4  sub-imove1101_ses-1_acq-pcasl_dir-lr_epi.json          /func/sub-imove1101_ses-1_task-rest_acq-pcasl_bold.nii.gz    True
+# 5  sub-imove1101_ses-1_acq-pcasl_dir-rl_epi.json          /func/sub-imove1101_ses-1_task-rest_acq-pcasl_bold.nii.gz    True
 
 if [[ ! $(basename ${PWD}) == "fmap" ]]; then
     echo
@@ -24,22 +24,17 @@ fi
 echo
 echo
 
-find ../func/ -name "*task-restmbepi_acq-mbepi_bold.nii.gz" | sort | xargs fmap_intended_for.py -i *_acq-mbepi_dir-{lr,rl}_epi.json --overwrite -v
+find ../func/ -name "*task-rest_acq-epi_rec-topup_bold.gz" | sort | xargs fmap_intended_for.py -i *_acq-epse_dir-{ap,pa}_epi.json --overwrite -v
 
 echo
 echo
 
-find ../func/ -name "*task-restpcasl_acq-pcasl_bold.nii.gz" | sort | xargs fmap_intended_for.py -i *_acq-pcasl_dir-{lr,rl}_epi.json --overwrite -v
+find ../func/ -name "*task-rest_acq-mbepi_bold.nii.gz" | sort | xargs fmap_intended_for.py -i *_acq-mbepi_dir-{lr,rl}_epi.json --overwrite -v
 
 echo
 echo
 
-find ../func/ -name "*_task-restepifmap_acq-epi_rec-fmap_bold.nii.gz" | sort | xargs fmap_intended_for.py -i *acq-bold_phasediff.json --overwrite -v -f
-
-echo
-echo
-
-find ../func/ -name "*_task-restepitopup_acq-epi_rec-topup_bold.nii.gz" | sort | xargs fmap_intended_for.py -i *_acq-epse_dir-{ap,pa}_epi.json --overwrite -v
+find ../func/ -name "*_task-rest_acq-pcasl_bold.nii.gz" | sort | xargs fmap_intended_for.py -i *_acq-pcasl_dir-{lr,rl}_epi.json --overwrite -v
 
 echo
 echo
